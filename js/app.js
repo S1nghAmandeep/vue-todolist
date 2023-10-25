86a2a271c0b98e9c0e98c41d85f5e9c0e874972a
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-25 14:37:16 +0200

    added-base-properties

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    },
+    methoh(){
+
+    },
+
+    mounted(){
+        console.log('ok')
+    }
+
+}).mount('#app')
