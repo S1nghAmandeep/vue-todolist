d4b0f064f035ae18a81e3cd29a35743ee5a628d7
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-25 17:59:06 +0200

    added-bonus-files

---
+++ bonus/js/app.js
@@ -0,0 +1,41 @@
+
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            newTask: '',
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask !== '') {
+                this.todos.push({
+                    text: this.newTask,
+                    done: false,
+                })   
+            }
+            this.newTask = '';
+        }
+    },
+
+    mounted(){
+        console.log('ok')
+    }
+
+}).mount('#app')
